a1435168c2676ac84deaad638134538b0584e99d
Hoon <yanghoon5@gmail.com>
2025-07-26 18:04:37 +0900

    Build Flink Docker Image using Jib Gradle Plugin

--- tools/flink/sql/test.sql
+++ tools/flink/sql/test.sql
@@ -9,6 +9,7 @@ show current database;
 use my_db;
 
 show tables;
+-- show create table test_table;
 -- CREATE TABLE test_table (id BIGINT, data STRING);
 INSERT INTO test_table VALUES (1, 'foo'), (2, 'bar');
 SELECT * FROM test_table;
